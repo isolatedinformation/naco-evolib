5fa8d79bf52ebb6c9a0f92c3a99b814835d00001
varunseshadri <varunseshadri@gmail.com>
2021-12-15 13:52:09 +0100

    instantiated library locally

--- evolib.py
+++ evolib.py
@@ -1,8 +1,23 @@
+"""
+Evolutionary Library
+--------------------
+
+Contains classes defining a Individual, Population, Historical Population
+"""
+from typing import Type
+from typing_extensions import Self
 from evobasics import *
+from problib import Problem
+
 
 class Individual:
+    """
+    Class that defines an individual.
+    """
+
     next_id = 0
-    def __init__(self, problem_instance, genome=None, children=[], **kwargs):
+
+    def __init__(self, problem_instance: Type[Problem], genome=None, children=[], **kwargs):
         self.args = kwargs
         self.problem_instance = problem_instance
         self.genome = genome or problem_instance.generate()
@@ -10,34 +25,40 @@ class Individual:
         self.parents = []
         self.initialize_more(**self.args)
         self.fitness = None
-        self.id = Individual.next_id
+        self.id = Individual.next_id  # Create a unique ID for each individual
         self.age = 0
         Individual.next_id += 1
         HistoricalPopulation.historical_individuals[self.id] = self
-    
-    def problem(self):
+
+    def problem(self) -> Type[Problem]:
         return self.problem_instance
-    
+
     def initialize_more(self, **kwargs):
         pass
-    
-    def clone(self):
+
+    def clone(self) -> Self:
         new_individual = type(self)(self.problem_instance, cp(self.genome), **self.args)
         return new_individual
-    
-    def mutate(self):
-        new_individual = type(self)(self.problem_instance, self.problem_instance.mutate(self.genome), **self.args)
+
+    def mutate(self) -> Self:
+        new_individual = type(self)(
+            self.problem_instance, self.problem_instance.mutate(self.genome), **self.args
+        )
         new_individual.parents = [self.id]
         self.children += [new_individual.id]
         return new_individual
-    
+
     def recombine(self, other):
-        new_individual = type(self)(self.problem_instance, self.problem_instance.recombine(self.genome, other.genome), **self.args)
+        new_individual = type(self)(
+            self.problem_instance,
+            self.problem_instance.recombine(self.genome, other.genome),
+            **self.args
+        )
         new_individual.parents = [self.id, other.id]
         self.children += [new_individual.id]
         other.children += [new_individual.id]
         return new_individual
-    
+
     def evaluate(self, population):
         self.fitness = self.compute_fitness(population)
         return self.fitness
@@ -47,50 +68,52 @@ class Individual:
 
     def compute_fitness(self, population):
         return self.compute_problem_fitness()
-        
+
     def compute_secondary_fitness(self, population):
         return 0
 
     def compute_problem_fitness(self):
         return self.problem_instance.evaluate(self.genome)
-    
+
     def to_str(self):
-        result = ''
-        result += '<' + str(self.__class__.__name__) 
-        result += ' ' + str(self.genome)
-        result += str(self.to_str_more() or '')
-        result += ' @ ' + str(self.compute_problem_fitness()) 
-        result += ' (' + str(self.get_fitness())
-        result += ')>'
+        result = ""
+        result += "<" + str(self.__class__.__name__)
+        result += " " + str(self.genome)
+        result += str(self.to_str_more() or "")
+        result += " @ " + str(self.compute_problem_fitness())
+        result += " (" + str(self.get_fitness())
+        result += ")>"
         return result
-        
+
     def to_str_more(self):
-        return ''
-    
+        return ""
+
     def __str__(self):
         return self.to_str()
 
 
 class Population:
-    def __init__(self, problem_instance, individuals=None, **kwargs):
+    def __init__(self, problem_instance: Type[Problem], individuals=None, **kwargs):
         self.args = kwargs
         self.problem_instance = problem_instance
         if type(individuals) == list:
-            self.individual_class = kwargs.get('individual_class', (len(individuals) > 0) and type(individuals[0]) or Individual)
+            self.individual_class = kwargs.get(
+                "individual_class", (len(individuals) > 0) and type(individuals[0]) or Individual
+            )
             self.individuals = individuals
         elif type(individuals) == int:
-            self.individual_class = kwargs.get('individual_class', Individual)
+            self.individual_class = kwargs.get("individual_class", Individual)
             self.individuals = self.generate_all(individuals)
         else:
             self.individuals = []
         self.size = len(self.individuals)
         self.age = 0
-        self.mig_rate = self.args.get('mig', 0.1)
-        self.mut_rate = self.args.get('mut', 0.1)
-        self.rec_rate = self.args.get('rec', 0.3)
-        self.par_rate = self.args.get('par', 0.3)
-        self.selection = self.args.get('selection', select_by_cutoff)
-        self.selection_args = self.args.get('selection_args', {})
+        self.mig_rate = self.args.get("mig", 0.1)
+        self.mut_rate = self.args.get("mut", 0.1)
+        self.rec_rate = self.args.get("rec", 0.3)
+        self.par_rate = self.args.get("par", 0.3)
+        self.selection = self.args.get("selection", select_by_cutoff)
+        self.selection_args = self.args.get("selection_args", {})
         self.selection_args.update(dict(problem_instance=self.problem_instance))
         self.initialize_more(**self.args)
 
@@ -99,7 +122,7 @@ class Population:
 
     def generate(self):
         return self.individual_class(self.problem_instance, **self.args)
-        
+
     def generate_all(self, size):
         return [self.generate() for _ in range(size)]
 
@@ -108,7 +131,10 @@ class Population:
 
     def evaluate(self):
         current_population = cp(self)
-        self.individuals.sort(reverse=self.problem_instance.maximizing(), key=lambda individual: individual.evaluate(current_population))
+        self.individuals.sort(
+            reverse=self.problem_instance.maximizing(),
+            key=lambda individual: individual.evaluate(current_population),
+        )
         return self
 
     def select(self, size=None):
@@ -116,10 +142,10 @@ class Population:
         self.evaluate()
         self.individuals = self.selection(self.individuals, size, **self.selection_args)
         return self
-    
+
     def choose_mate(self, individual):
         return self.individuals[randi(max=int(self.par_rate * self.size))]
-        
+
     def choose_random(self):
         return self.individuals[randi(max=self.size)]
 
@@ -128,33 +154,40 @@ class Population:
 
     def mutate(self):
         # self.individuals = [randb(self.mut_rate) and self.invoke_mutation(individual) or individual for individual in self.individuals]
-        self.individuals += [self.invoke_mutation(individual) for individual in self.individuals if randb(self.mut_rate)]
+        self.individuals += [
+            self.invoke_mutation(individual)
+            for individual in self.individuals
+            if randb(self.mut_rate)
+        ]
         return self
-    
+
     def invoke_recombination(self, individual):
         return individual.recombine(self.choose_mate(individual))
-    
+
     def recombine(self):
         self.evaluate()
-        self.individuals += [self.invoke_recombination(individual) for individual in self.individuals if randb(self.rec_rate)]
+        self.individuals += [
+            self.invoke_recombination(individual)
+            for individual in self.individuals
+            if randb(self.rec_rate)
+        ]
         return self
-        
+
     def augment(self):
         self.individuals += [self.generate() for _ in range(int(self.mig_rate * self.size))]
         return self
-        
+
     def evolve(self):
         new_population = type(self)(self.problem_instance, self.individuals, **self.args)
         new_population.age += 1
         new_population.problem_instance.update(self.age)
         for individual in new_population.individuals:
             individual.age += 1
-        new_population.recombine()        
-        new_population.mutate()        
-        new_population.augment()        
+        new_population.recombine()
+        new_population.mutate()
+        new_population.augment()
         new_population.select()
         return new_population
-
 
 
 class HistoricalPopulation(Population):
@@ -162,8 +195,10 @@ class HistoricalPopulation(Population):
     historical_opt_fitnesses = {}
     historical_individuals = {}
     historical_ancestors = {}
+
     def initialize_more(self, **kwargs):
         pass
+
     def compute_pf(self, individual, depth=0):
         if individual.id in HistoricalPopulation.historical_avg_fitnesses:
             return HistoricalPopulation.historical_avg_fitnesses[individual.id]
@@ -172,10 +207,15 @@ class HistoricalPopulation(Population):
         else:
             children_pf = []
             for child_id in individual.children:
-                children_pf += [self.compute_pf(HistoricalPopulation.historical_individuals[child_id], depth+1)]
+                children_pf += [
+                    self.compute_pf(
+                        HistoricalPopulation.historical_individuals[child_id], depth + 1
+                    )
+                ]
             avg_productive_fitness = avg(children_pf)
             HistoricalPopulation.historical_avg_fitnesses[individual.id] = avg_productive_fitness
             return avg_productive_fitness
+
     def compute_opf(self, individual, depth=0):
         if individual.id in HistoricalPopulation.historical_opt_fitnesses:
             return HistoricalPopulation.historical_opt_fitnesses[individual.id]
@@ -184,10 +224,17 @@ class HistoricalPopulation(Population):
         else:
             children_pf = []
             for child_id in individual.children:
-                children_pf += [self.compute_opf(HistoricalPopulation.historical_individuals[child_id], depth+1)]
-            opt_productive_fitness = self.problem_instance.maximizing() and max(children_pf) or min(children_pf)
+                children_pf += [
+                    self.compute_opf(
+                        HistoricalPopulation.historical_individuals[child_id], depth + 1
+                    )
+                ]
+            opt_productive_fitness = (
+                self.problem_instance.maximizing() and max(children_pf) or min(children_pf)
+            )
             HistoricalPopulation.historical_opt_fitnesses[individual.id] = opt_productive_fitness
             return opt_productive_fitness
+
     def compute_pf_here(self, ancestor):
         descendant_count = 0
         descendant_total_fitness = 0
@@ -199,6 +246,7 @@ class HistoricalPopulation(Population):
             return float(descendant_total_fitness) / float(descendant_count)
         else:
             return None
+
     def compute_opf_here(self, ancestor):
         descendant_count = 0
         descendant_best_fitness = self.problem_instance.maximizing() and -9999999 or 9999999
@@ -216,20 +264,33 @@ class HistoricalPopulation(Population):
             return float(descendant_best_fitness)
         else:
             return None
+
     def is_ancestor(self, ancestor, descendant):
         if str(descendant.id) + " " + str(ancestor.id) in HistoricalPopulation.historical_ancestors:
-            return HistoricalPopulation.historical_ancestors[str(descendant.id) + " " + str(ancestor.id)]
+            return HistoricalPopulation.historical_ancestors[
+                str(descendant.id) + " " + str(ancestor.id)
+            ]
         if descendant.id in ancestor.children:
             return True
         else:
             for child_id in ancestor.children:
-                if self.is_ancestor(HistoricalPopulation.historical_individuals[child_id], descendant):
-                    HistoricalPopulation.historical_ancestors[str(descendant.id) + " " + str(ancestor.id)] = True
+                if self.is_ancestor(
+                    HistoricalPopulation.historical_individuals[child_id], descendant
+                ):
+                    HistoricalPopulation.historical_ancestors[
+                        str(descendant.id) + " " + str(ancestor.id)
+                    ] = True
                     return True
-                HistoricalPopulation.historical_ancestors[str(descendant.id) + " " + str(ancestor.id)] = False
+                HistoricalPopulation.historical_ancestors[
+                    str(descendant.id) + " " + str(ancestor.id)
+                ] = False
             return False
+
     def filter_descendants(self, ancestor):
-        return [individual for individual in self.individuals if self.is_ancestor(ancestor, individual) ]
+        return [
+            individual for individual in self.individuals if self.is_ancestor(ancestor, individual)
+        ]
+
     def best_pf(self):
         best_pf = self.problem_instance.maximizing() and -9999999 or 9999999
         pfs = []
@@ -237,10 +298,13 @@ class HistoricalPopulation(Population):
         for individual in self.individuals:
             pf = self.compute_pf(individual)
             pfs += [pf]
-            if (self.problem_instance.maximizing() and pf > best_pf) or (not self.problem_instance.maximizing() and pf < best_pf):
+            if (self.problem_instance.maximizing() and pf > best_pf) or (
+                not self.problem_instance.maximizing() and pf < best_pf
+            ):
                 best_pf = pf
                 best = individual
         return best_pf
+
     def best_opf(self):
         best_pf = self.problem_instance.maximizing() and -9999999 or 9999999
         pfs = []
@@ -248,22 +312,30 @@ class HistoricalPopulation(Population):
         for individual in self.individuals:
             pf = self.compute_opf(individual)
             pfs += [pf]
-            if self.problem_instance.maximizing() and pf > best_pf or not self.problem_instance.maximizing() and pf < best_pf:
+            if (
+                self.problem_instance.maximizing()
+                and pf > best_pf
+                or not self.problem_instance.maximizing()
+                and pf < best_pf
+            ):
                 best_pf = pf
                 best = individual
         return best_pf
+
     def best_f(self):
         if self.problem_instance.maximizing():
             return max([individual.compute_problem_fitness() for individual in self.individuals])
         else:
             return min([individual.compute_problem_fitness() for individual in self.individuals])
 
+
 class TestPopulation(Population):
     def initialize_more(self, **kwargs):
-        self.subjects = self.args.get('subjects', [])
+        self.subjects = self.args.get("subjects", [])
+
     def select(self, size=None):
         size = size or self.size
         self.evaluate()
-        self.individuals = self.individuals[0:(size-len(self.subjects))]
+        self.individuals = self.individuals[0 : (size - len(self.subjects))]
         self.individuals += self.subjects
         return self
